ebc06c227f834a256ce26e3489c23cf7f4d2d421
Gustavo Pagano <gustavopagano03@gmail.com>
2021-10-20 15:50:19 -0300

    hasta 7°

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -27,15 +27,16 @@ const nuevoModulo = 21 % 5 === 1;
 
 function devolverString(str) {
   // "Return" la string provista: str
-  // Tu código: return str;
-  
+  // Tu código: 
+  return str; 
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  var suma = x + y;
+  return suma;
 }
 
 function resta(x, y) {
