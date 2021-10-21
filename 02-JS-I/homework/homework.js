e079343e6188426e92c642af6b114df507da20a0
Gustavo Pagano <gustavopagano03@gmail.com>
2021-10-21 00:08:12 -0300

    revisión5

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -197,7 +197,8 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-    return ('hola ' + nombre + '!');
+  var saludo = ('hola ' + nombre + '!');
+    return saludo
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -236,9 +237,9 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if (letra.length > 1){
-    return "Dato Incorrecto";
+    return "Dato incorrecto";
   }
-  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
+  if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
     return "Es vocal";
   }
   return "Dato Incorrecto";
