c9b76b2639f1a37adee80f97e4ad106632adf7e5
Gustavo Pagano <gustavopagano03@gmail.com>
2021-10-20 23:26:48 -0300

    saludo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -189,14 +189,16 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  var combinado = nombre + " " + apellido;
+  return (combinado);
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  var saludo = ('hola ' + nombre + '!')
+  return saludo;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
