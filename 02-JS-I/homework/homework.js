10de616a3f36cd6ac7d258a3dc810ecf18306dac
Gustavo Pagano <gustavopagano03@gmail.com>
2021-10-27 17:12:32 -0300

    test03_03

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -85,6 +85,16 @@ function menosQueNoventa(num) {
  }
 }
 
+function menosQueNoventa(num) {
+  // Devuelve "true" si el argumento de la función "num" es menor que noventa
+  // De lo contrario, devuelve "false"
+  // Tu código:
+  if (num < 90) {
+    return true;
+} {
+  return false;
+ }
+}
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
@@ -235,7 +245,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra.length > 1){
+  if (letra.length > 1) {
     return "Dato incorrecto";
   }
   if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
