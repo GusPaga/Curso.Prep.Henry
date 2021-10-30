d95322b100d53de73025c209703a952a653f8a4f
Gustavo Pagano <gustavopagano03@gmail.com>
2021-10-30 18:59:15 -0300

    test03_08

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -170,7 +170,11 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  if (valor = true){
+    return ("Soy verdadero");
+  } else {
+    return ("Soy falso");
+  }
 }
 
 function tablaDelSeis(){
@@ -183,13 +187,25 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+ if (numero > 99 && numero < 1000) {
+   return true;
+   } else {
+     return false;
+   } 
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var a = numero;
+  var i = 0;
+  do  {
+    i = i + 1;
+    a = a + 5;
+  }
+  while (i < 8);
+  return a;
 }
 
 
